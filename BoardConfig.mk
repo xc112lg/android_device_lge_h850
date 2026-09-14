5a01a015897cb9f6e7bdd50d0853bbbc6fa63936
xc112lg <152837944+xc112lg@users.noreply.github.com>
2026-09-14 16:36:13 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,8 +28,6 @@ TARGET_KERNEL_CONFIG += vendor/lge/h850.config
 # Partitions
 # Override this after inheriting g5 common size
 # h850 system image partition is sized differently than US variants
-BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4148166656
 
 # inherit from the proprietary version
 include vendor/lge/h850/BoardConfigVendor.mk
